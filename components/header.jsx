d2a4ebee40884a026ae5ac6f043a3d1b1c623085
meghalakhwan <meghalakhwan9@gmail.com>
2025-03-08 23:45:08 +0530

    Add main

--- components/header.jsx
+++ components/header.jsx
@@ -23,7 +23,9 @@ import {
   DropdownMenuContent,
 } from "@radix-ui/react-dropdown-menu";
 
-const Header = () => {
+import { checkUser } from "@/lib/checkUser";
+const Header = async() => {
+  await checkUser();
   return (
     <header
       className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50
